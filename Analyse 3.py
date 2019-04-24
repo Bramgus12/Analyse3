609556fc1b837b2b3c92a68507333d0e58e6dcd5
Bram Gussekloo <0966476@hr.nl>
2019-04-24 14:15:08 +0200

    added complex search

--- Analyse 3.py
+++ Analyse 3.py
@@ -2,13 +2,21 @@ import json
 Userinput = input("Search for a book: ")
 with open('books.json', 'r') as json_file:
     data = json.load(json_file)
+    s1 = ''
     for d in data:
-        a = str(d)
-        b = a.lower()
-        c = Userinput.lower()
-        if c in b:
-            print(d)
-
+        dataString = str(d)
+        lowercasedObject = dataString.lower()
+        lowercasedInput = Userinput.lower()
+        splittedInput = lowercasedInput.split()
+        print(str(splittedInput))
+        if lowercasedInput in lowercasedObject:
+            s = d["author"]+  ", " + d["country"]+  ", " + d["imageLink"]+  ", " + d["language"]+  ", " + d["link"]+  ", " + str(d["pages"])+  ", " + d["title"]+  " and "+ str(d["year"]) + "\n" + "8=======================================================================D~~~~~~" + "\n"
+            if s in s1:
+                print("")
+            else:
+                s1 = s1 + s
+    print(s1)
+    print()
 
 # class Book:
 #     def __init__(self):
